8d735b83f6bb63174ba01f8017510ed7d8c26b11
newgraph-lschick <lucy@newgraphenvironment.com>
2025-01-20 13:57:25 -0800

    loaded watershed stats. Will need to do again once https://github.com/NewGraphEnvironment/fish_passage_skeena_2024_reporting/issues/18 is complete.

---
+++ scripts/02_reporting/0170-load-wshd_stats.R
@@ -0,0 +1,189 @@
+# retrieve the watershed stats and elevations of the pscis sites then burn to the sqlite
+
+# Load required objects -------------------------------------------------
+
+# 0165-read-sqlite.R` reads in the `bcfishpass` object
+source("scripts/02_reporting/0165-read-sqlite.R")
+
+# `tables.R`read in the `pscis_all_sf` object
+source("scripts/tables.R")
+
+
+## Filter the bcfishpass data to just the phase 2 sites -------------------------------------------------
+
+# Skeena 2024 issue:
+# crossing 58264 is located on Simpson creek, which is incorrectly mapped in BC FWA, therefore the crossing is not tied to a modelled crossing.
+# The correct modelled crossing is tied to PSCIS crossing 58258, which is on a super small trib.
+# I think we need to force the linkage of PSCIS crossing 58264 to modelled crossing 1800143, using bcdfishpass csvs.
+# For now, I will just filter `bcfishpass_phase2` to include modelled crossing 1800143.
+
+bcfishpass_phase2 <- bcfishpass |>
+  dplyr::filter(
+    stringr::str_detect(
+      stream_crossing_id,
+      paste0(pscis_phase2 |>
+               pull(pscis_crossing_id),
+             collapse = "|")
+    ) | modelled_crossing_id == "1800143"
+  )
+
+
+#NOTE!!!! - we don't actually need everything from the tables script  the pscis_all_sf objects.
+
+## Remove crossings on first order streams -------------------------------------------------
+# we needed to remove crossings that are first order because the fwapgr api kicks us off
+
+bcfishpass_phase2_clean <- bcfishpass_phase2 |>
+  dplyr::filter(stream_order != 1)
+
+# for this years data there is none
+bcfishpass_phase2_1st_order <- bcfishpass_phase2 |>
+  dplyr::filter(stream_order == 1)
+
+
+
+## Remove first order crossings if needed -------------------------------------------------
+
+# conn <- DBI::dbConnect(
+#   RPostgres::Postgres(),
+#   dbname = dbname,
+#   host = host,
+#   port = port,
+#   user = user,
+#   password = password
+# )
+#
+# dat <- bcfishpass_phase2 |>
+#   filter(stream_order == 1)
+#   # distinct(localcode_ltree, .keep_all = T)
+#
+# # add a unique id - we could just use the reference number
+# dat$misc_point_id <- seq.int(nrow(dat))
+#
+#
+# # # lets get our fist order watersheds
+# # dat <- bcfishpass_phase2 |>
+# #   filter(stream_order == 1)
+#
+# ##pull out the localcode_ltrees we want
+# ids <-  dat |>
+#   pull(localcode_ltree) |>
+#   # unique() |>
+#   as_vector() |>
+#   na.omit()
+#
+# ids2 <- dat |>
+#   pull(wscode_ltree) |>
+#   # unique() |>
+#   as_vector() |>
+#   na.omit()
+#
+# # note that we needed to specifiy the order here.  Not sure that is always going to save us....
+# sql <- glue::glue_sql(
+#
+#                                 "SELECT localcode_ltree, wscode_ltree, area_ha, geom as geometry from whse_basemapping.fwa_watersheds_poly
+#                                 WHERE localcode_ltree IN ({ids*})
+#                                 AND wscode_ltree IN ({ids2*})
+#                                 AND watershed_order = 1
+#                                 ",
+#   .con = conn
+# )
+#
+# wshds_1ord_prep <- sf::st_read(conn,
+#                         query = sql) |>
+#   st_transform(crs = 4326)
+#
+#
+# # test <- wshds_1ord_prep |>
+# #   filter(localcode_ltree == wscode_ltree)
+#
+#
+# # i think we need to be joining in the stream_crossing_id and joining on that....
+#
+# wshds_1ord <- left_join(
+#   dat |>
+#     distinct(stream_crossing_id, .keep_all = T) |>
+#     select(
+#     stream_crossing_id,
+#     localcode_ltree,
+#     wscode_ltree,
+#     stream_order
+#     ) |>
+#     mutate(stream_crossing_id = as.character(stream_crossing_id)),
+#     # filter(stream_order == 1),
+#   wshds_1ord_prep |>
+#     mutate(localcode_ltree = as.character(localcode_ltree),
+#            wscode_ltree = as.character(wscode_ltree)),
+#   by = c('localcode_ltree','wscode_ltree')
+# )
+
+
+
+## Extract the watershed data -------------------------------------------------
+
+# call fwapgr
+wshds_fwapgr <- fpr::fpr_sp_watershed(bcfishpass_phase2_clean)
+
+# If there was first order watersheds, then combine the following:
+# wshds_combined <- bind_rows(
+#   wshds_fwapgr,
+#   wshds_1ord
+# )
+
+
+
+## Calculate the watershed stats -------------------------------------------------
+wshds_raw <- fpr::fpr_sp_wshd_stats(dat = wshds_fwapgr) |>
+  dplyr::mutate(area_km = round(area_ha/100, 1)) |>
+  dplyr::mutate(dplyr::across(contains('elev'), round, 0)) |>
+  dplyr::arrange(stream_crossing_id)
+
+
+## Skeena 2024- not run yet because pscis data has not been accepted yet which is needed for `pscis_all_sf` object.
+## Tried to work around this but no luck and taking too much time, so just renamed `wshds <- wshds_raw` and moved on.
+# wshds <-wshds_raw
+
+# add in the elevation of the site
+wshds <-  dplyr::left_join(
+  wshds_raw |> dplyr::mutate(stream_crossing_id = as.numeric(stream_crossing_id)),
+
+  pscis_all_sf |> dplyr::distinct(pscis_crossing_id, .keep_all = T) |>
+    sf::st_drop_geometry() |>
+    dplyr::select(pscis_crossing_id, elev_site = elev),
+
+  by = c('stream_crossing_id' = 'pscis_crossing_id')) |>
+  # put elev_site before elev_min
+  dplyr::relocate(elev_site, .before = elev_min)
+
+
+
+## Add to the geopackage -------------------------------------------------
+path_gis_wshds <- fs::path("~/Projects/gis/sern_skeena_2023/data_field/2024/fishpass_mapping.gpkg")
+
+wshds |>
+  sf::st_write(dsn = path_gis_wshds,
+               layer = 'hab_wshds',
+               delete_layer = T,
+               append = F) ##might want to f the append....
+
+
+
+## Burn to a kml -------------------------------------------------
+#burn to kml as well so we can see elevations
+sf::st_write(wshds |>
+        rename(name = stream_crossing_id),
+         append = F,
+         delete_layer = T,
+         driver = 'kml',
+         dsn = "data/inputs_extracted/wshds.kml")
+
+
+
+## Add to the sqlite -------------------------------------------------
+conn <- readwritesqlite::rws_connect("data/bcfishpass.sqlite")
+readwritesqlite::rws_list_tables(conn)
+readwritesqlite::rws_drop_table("wshds", conn = conn) ##now drop the table so you can replace it
+readwritesqlite::rws_write(wshds, exists = F, delete = TRUE,
+          conn = conn, x_name = "wshds")
+readwritesqlite::rws_list_tables(conn)
+readwritesqlite::rws_disconnect(conn)
