a7dd27de8064a3138452b0e3d0d90ae00fdc23d1
newgraph-lschick <lucy@newgraphenvironment.com>
2025-03-13 15:32:55 -0700

    dont commit html

--- scripts/run.R
+++ scripts/run.R
@@ -7,6 +7,7 @@ preview_chapter('0100-intro.Rmd')
 
 # move over the photos from shared drive to repo and update metadata so that the photos are included in the report interactive map
 rmarkdown::render('scripts/02_reporting/photos_import.Rmd')
+usethis::use_git_ignore('scripts/02_reporting/photos_import.html')
 source('scripts/02_reporting/0180-photos-extract-metadata.R')
 
 
